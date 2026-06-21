217df789bf6425d6759e40bbad44a3062c7671b1
Gef <clsyfriday@gmail.com>
2026-06-21 14:32:23 +0100

    style: change website logo

--- frontend/components/site/landing-nav.tsx
+++ frontend/components/site/landing-nav.tsx
@@ -30,14 +30,12 @@ export function LandingNav() {
         href="#"
         className="flex items-center gap-2 bg-neutral-900/90 backdrop-blur rounded-full pl-4 pr-6 py-3"
       >
-        <svg
-          viewBox="0 0 256 256"
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="/arktion-icon-dark.svg"
+          alt="Arktion"
           className="h-5 w-5"
-          fill="#ffffff"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path d="M 128 192 L 128 256 L 64.5 256 L 32 223 L 0 192 L 0 128 L 64 128 Z M 256 192 L 256 256 L 192.5 256 L 160 223 L 128 192 L 128 128 L 192 128 Z M 128 64 L 128 128 L 64.5 128 L 32 95 L 0 64 L 0 0 L 64 0 Z M 256 64 L 256 128 L 192.5 128 L 160 95 L 128 64 L 128 0 L 192 0 Z" />
-        </svg>
+        />
         <span className="text-white text-sm font-normal tracking-tight">arktion</span>
       </a>
 
